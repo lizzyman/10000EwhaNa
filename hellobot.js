0cf46c234c29e5258665199ff9c36a807f8c6479
차유경 <chayukyung@chayugyeong-ui-MacBook-Pro.local>
2016-09-24 19:31:12 +0900

    test

--- hellobot.js
+++ hellobot.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
 	text : 'Hello, ' + userName + '!\n' +
-"This is how to Manewhana module.\n  *help : to help you to use useful commands of Manewhana.\n  *fec [day1][empty classes*] [day2][empty classes] : to find empty day and empty classes (ex. *fec 월45 화26 목23)\n  *fecr [day] [class] [builing] : to find empty builings(ex. *fecr 월 4 ecc)"
+'This is how to Manewhana module.\n  *help : to help you to use useful commands of Manewhana.\n  *fec [day1][empty classes*] [day2][empty classes] : to find empty day and empty classes (ex. *fec 월45 화26 목23)\n  *fecr [day] [class] [builing] : to find empty builings(ex. *fecr 월 4 ecc)'
 
   };
 
