1c156de559bb635a6c388fd0cd3c3921c32c2ac1
차유경 <chayukyung@chayugyeong-ui-MacBook-Pro.local>
2016-09-24 18:06:21 +0900

    test

--- hellobot.js
+++ hellobot.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello, ' + userName + '!!!!!!'
+    text : 'Hello, ' + userName + '/nI'도와줄게'
   };
 
   // avoid infinite loop
